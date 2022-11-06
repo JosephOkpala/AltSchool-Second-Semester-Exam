986383c514c0e43b369e26f2bfbac31e75c77639
Okpala Joseph <105152644+JosephOkpala@users.noreply.github.com>
2022-11-07 00:13:32 +0100

    Update RepoDetails.jsx

--- src/components/RepoDetails.jsx
+++ src/components/RepoDetails.jsx
@@ -1,38 +1,12 @@
-import { useEffect } from "react"
-import { useState } from "react"
 import { useParams } from "react-router-dom"
 
-export const RepoDetails = () => {
-    // const [details, setDetails] = useState([])
-
-    // useEffect(() => {
-    //     const fetchRepos = async () => {
-    //         try {
-    //           const response = await fetch(
-    //             "https://api.github.com/users/JosephOkpala/repos"
-    //           );
-    //           const data = await response.json();
-    //           setDetails(data);
-    //         } catch (error) {
-    //           console.log("error");
-    //         }
-    //       };
-    //       fetchRepos();
-    // },[])
-
+const RepoDetails = () => {
     const { repoId } = useParams()
     console.log({repoId})
     return(
         <div style={{color:'white'}}>
             <h1>Repo Details</h1>
             <h1>{repoId}</h1>
-            {/* <p>{name}</p> */}
-            {/* <h1>Repo Details {repoId}</h1>
-            <div>{details.map((detail) =>(
-                <div key={repoId}>
-                    <h1>{detail.owner.login}</h1>
-                </div>
-            ))}</div> */}
         </div>
     )
 }
